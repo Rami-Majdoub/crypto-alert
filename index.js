dc8397ca608633c88ae1a088f3f6f756b67aa3a8
rami-majdoub <rami.majdoub1@gmail.com>
2023-07-15 18:48:27 +0100

    feat: take requests from args

--- index.js
+++ index.js
@@ -5,42 +5,22 @@ const Preferences = require("preferences");
 const path = require('node:path');
 const { Epoch } = require("@jmac18/epoch");
 
-// String
-// notifier.notify('Message');
+const { requests, dev } = require('minimist')(process.argv.slice(2));
 
-// Object
-/*
-notifier.notify({
-  title: 'My notification',
-  message: 'Hello, there!'
-});
-*/
-
-if(process.env.API_KEY == undefined){
-  console.log("Argument apikey is required (usage: --apikey=\"XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX\" get your api key from https://pro.coinmarketcap.com/account)");
-  return;
-}
-
-
-// https://coinmarketcap.com/api/documentation/v1/#operation/getV2ToolsPriceconversion
-getConversionRate = async ({ from, to }) => {
+const getConversionRate = async ({ from, to }) => {
   let response;
   try {
-    response = await axios.get(
-      `https://pro-api.coinmarketcap.com/v2/tools/price-conversion?convert=${to}&symbol=${from}&amount=1`,
-      {
-        headers: {
-          'X-CMC_PRO_API_KEY': process.env.API_KEY,
-        },
-      }
-    );
+    response = await axios.get(`https://api.coingecko.com/api/v3/coins/${from.toLowerCase()}`);
   } catch(ex) {
-    console.log(`1 ${from.padEnd(10)} = ${to}: error(${ex.message})`);
+    console.log(`1 ${from.padEnd(10)} = ${to.toUpperCase()}: error(${ex.message})`);
   }
   if (response) {
     // success
-    const [ result ] = response.data.data;
-    const rate = result.quote[to].price;
+    const resNameTo = to.toUpperCase();
+    const resNameFrom = response.data.symbol.toUpperCase();
+    const resPrice = response.data.market_data.current_price[to.toLowerCase()];
+    
+    const rate = resPrice;
     return rate;
   }
 }
@@ -55,32 +35,38 @@ const overridePrefs = (prefs) => prefs.last_check = Epoch().days(0).hours(24 +1)
 const load = (prefs) => Epoch().days(0).hours(24).ago > Epoch(prefs.last_check || 0).now;
 const save = (prefs) => prefs.last_check = Epoch().now;
 
-async function main(){
-	const prefs = loadPrefs();
+const showAlert= (request) => {
+	notifier.notify({
+	  title: 'Crpto alert (Conversion rate satisfied)',
+	  message: request
+	});
+}
+
+const prefs = loadPrefs();
+console.log(`date: ${Epoch().now}`);
+
+if(!dev){
+	const canrun = load(prefs);
+	if(!canrun) return;
+}
+
+const check = async (request) => {
+	const [from, operator, amount, to] = request.split(" ");
 	
-	// overridePrefs(prefs);
-	console.log(`date: ${Epoch().now}`);
-	
-	if( cooldown = false ){
-		const canrun = load(prefs);
-		if(!canrun) return;
-	}
-	
-	// BAT > 3.5 GRT:BTC < 20000 USD
-	const rate = await getConversionRate({ from: "BAT", to: "GRT" });
-	if (rate >= 2.25){
-		notifier.notify({
-		  title: 'Crpto alert',
-		  message: 'Conversion rate satisfied'
-		});
-		save(prefs);
+	const rate = await getConversionRate({ from, to });
+	console.log({ from, to, rate });
+	if (
+		operator == ">" && rate > amount
+		|| operator == "<" && rate < amount
+	){
+		showAlert(request);
+		if(!dev){
+			save(prefs);
+		}
 	}
 	console.log(`Rate: ${rate}`);
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+(async () => {
+	requests.replaceAll(", ", ",").split(",").forEach(async request => await check(request))
+})();
